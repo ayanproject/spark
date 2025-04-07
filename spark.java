2ace7d066d6e6909e733f3a6c6d9d378126feb2c
Ayan Mondal <papaim294@gmail.com>
2025-04-07 17:34:19 +0530

    sucess

--- spark.java
+++ spark.java
@@ -2,7 +2,7 @@ class spark{
     public static void main(String args[]){
         System.out.println("hello");
     }
-    public void m1(){
+    public void payment(){
         System.out.println("m1");
     }
 }
